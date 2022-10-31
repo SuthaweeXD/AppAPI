794ce31cc96662684208f4a319928681cf3d18eb
SuthaweeXD <suthawee.solaeh@gmail.com>
2022-10-31 16:12:04 +0700

    UpAccept

--- app/routes/orders.route.js
+++ app/routes/orders.route.js
@@ -1,6 +1,6 @@
 module.exports = (app) => {
   const router = require('express').Router()
-  const { create, findAll, findOne, reportOrder, reportAllorder, update, deleteOne, updateStatus, findOrder, paymentOrder, findPayment } = require('../controllers/orders.controller')
+  const { create, findAll, findOne, reportOrder, reportAllorder, update, deleteOne, updateStatus, findOrder, paymentOrder, findPayment, reportOrderAccept } = require('../controllers/orders.controller')
   const multer  = require('multer')
   const upload = multer()
   
@@ -14,7 +14,7 @@ module.exports = (app) => {
   router.get('/reportOrder/:startDate/:endDate',reportOrder)
   router.get('/reportAllOrder/:status/:startDate/:endDate',reportAllorder)
 //   router.post('/number',number)
-
+router.get('/reportOrderAccept/:startDate/:endDate',reportOrderAccept)
   router.put('/:id', update)
 
   router.put('/payment/:id',upload.single('photo'), paymentOrder)
